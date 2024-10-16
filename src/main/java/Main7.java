87e4a47322697963a0a6dcf5ff74af45b47ece85
OgarMikeDev <123175953+OgarMikeDev@users.noreply.github.com>
2024-10-16 19:48:22 +0300

    Update Main7.java

--- src/main/java/Main7.java
+++ src/main/java/Main7.java
@@ -8,17 +8,23 @@ public class Main7 {
         List<String> book = Files.readAllLines(
                 Paths.get(
                         "C:\\Users\\ogar_m\\IdeaProjects\\Numbers\\src\\main\\resources\\data\\Война и мир, том 1.txt"));
-        System.out.println(findTermFrequency(book));
+        String fullText = String.valueOf(book);
+        String[] allChapters = fullText.split("\n");
+        for (String currentChapter : allChapters) {
+            System.out.println(findTermFrequency(currentChapter));
+        }
+
     }
 
-    private static String findTermFrequency(List<String> book) throws Exception {
+    private static String findTermFrequency(String chapter) throws Exception {
+        List<String> chapterList = List.of(chapter);
         System.out.print("Введите слово, а мы выведем в консоль кол-во его повторений в данной книге: ");
         String inputWord = new Scanner(System.in).nextLine();
         int countAttempts = 0;
         int countWords = 0;
         double termFrequency = 0;
 
-        for (String line : book) {
+        for (String line : chapterList) {
             String[] elements = line.split("(\s+)|(\\n+)|(,)|(\\.)");
             for (String element : elements) {
                 if (element.compareToIgnoreCase(inputWord) == 0) {
@@ -31,12 +37,11 @@ public class Main7 {
             }
         }
 
-        termFrequency = (double)countAttempts / (double) countWords;
+        termFrequency = (double) countAttempts / (double) countWords;
 
         String response = "Кол-во повторений слова " + inputWord + " равно " + countAttempts +
                 "\nКол-во слов в книге равно " + countWords +
                 "\nЧастота потребления слова " + inputWord + " в книге равна " + termFrequency;
         return response;
-
     }
 }
